fce16480eb6764cc9132dcbaf0bf9100f44f8484
abdallahserag <abdoserag862@gmail.com>
2025-11-09 16:45:21 +0200

    add login and register and home routes and update the main

--- lib/Utils/routes_app.dart
+++ lib/Utils/routes_app.dart
@@ -1,5 +1,7 @@
 class RoutesApp {
   static const String loginRouteName = 'login';
+  static const String registerRouteName = 'register';
+  static const String homeRouteName = 'home';
   static const String onboardingRouteName = 'onboarding';
   static const String updateProfileRouteName = 'update_profile';
   static const String forgetPasswordScreen = 'forgetPassword';
