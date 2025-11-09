fba72863f44c064f6184a03f896170d529e03575
ahmed <ahmedsukkary52##@gmail.com>
2025-11-09 03:02:37 +0200

    + delete words from l10n

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -25,5 +25,131 @@ class AppLocalizationsEn extends AppLocalizations {
   String get update_profile_delete_account => 'Delete Account';
 
   @override
+  String get watch => 'Watch';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forget_password => 'Forget Password';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get find_favorite_movie => 'Find Your Next Favorite Movie Here';
+
+  @override
+  String get get_access_huge => 'Get access to a huge library of movies to suit all tastes. You will surely like it.';
+
+  @override
+  String get explore_now => 'Explore Now';
+
+  @override
+  String get discover_movies => 'Discover Movies';
+
+  @override
+  String get explore_vast => 'Explore a vast collection of movies in all qualities and genres. Find your next favorite film with ease.';
+
+  @override
+  String get back => 'Back';
+
+  @override
+  String get discover_movies_from_genre => 'Discover movies from every genre, in all available qualities. Find something new and exciting to watch every day.';
+
+  @override
+  String get explore_all_genres => 'Explore All Genres';
+
+  @override
+  String get create_watchlist => 'Create Watchlist';
+
+  @override
+  String get save_movies_to_list => 'Save movies to your watchlist to keep track of what you want to watch next. Enjoy films in various qualities and genres.';
+
+  @override
+  String get rate_review_learn => 'Rate, Review, and Learn';
+
+  @override
+  String get share_thoughts => 'Share your thoughts on the movies you\'ve watched. Dive deep into film details and help others discover great movies with your reviews.';
+
+  @override
+  String get start_watching => 'Start Watching Now';
+
+  @override
+  String get finish => 'Finish';
+
+  @override
+  String get dont_have_account => 'Don’t Have Account?';
+
+  @override
+  String get create => 'Create';
+
+  @override
+  String get or => 'OR';
+
+  @override
+  String get login_with_google => 'Login With Google';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get confirm_password => 'Confirm Password';
+
+  @override
+  String get phone_number => 'Phone Number';
+
+  @override
+  String get create_account => 'Create Account';
+
+  @override
+  String get already_have_account => 'Already Have Account?';
+
+  @override
+  String get verify_email => 'Verify Email';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get see_more => 'See More';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get wish_list => 'Wish List';
+
+  @override
+  String get history => 'History';
+
+  @override
+  String get edit_profile => 'Edit Profile';
+
+  @override
+  String get exit => 'Exit';
+
+  @override
+  String get watch_list => 'Watch List';
+
+  @override
+  String get reset_password => 'Reset Password';
+
+  @override
+  String get pick_avatar => 'Pick Avatar';
+
+  @override
+  String get delete_account => 'Delete Account';
+
+  @override
+  String get update_data => 'Update Data';
+
+  @override
   String get update_profile_update_data => 'Update Data';
 }
