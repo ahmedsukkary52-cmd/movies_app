7b2af8ef9d2cda84cab8b897e47cd1419a94ae2d
ahmedmostafa361 <ahmedmostafamm878@gmail.com>
2025-11-09 14:58:50 +0200

    Revert "+ add forget and reset password screen (Ui + validation)"

--- lib/Utils/routes_app.dart
+++ lib/Utils/routes_app.dart
@@ -1,6 +1,4 @@
 class RoutesApp {
   static const String loginRouteName = 'login';
   static const String onboardingRouteName = 'onboarding';
-  static const String forgetPasswordScreen = 'forgetPassword';
-  static const String resetPasswordScreen = 'resetPassword';
 }
