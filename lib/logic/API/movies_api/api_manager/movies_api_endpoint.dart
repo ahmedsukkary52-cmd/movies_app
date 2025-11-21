c8ac41ba6bd8641e4c15da947fcd320eb44e6564
ahmedmostafa361 <ahmedmostafamm878@gmail.com>
2025-11-21 03:56:32 +0200

    + Add MovieDetails API and model, fix null safety, and add error handling.

--- lib/logic/API/movies_api/api_manager/movies_api_endpoint.dart
+++ lib/logic/API/movies_api/api_manager/movies_api_endpoint.dart
@@ -1,3 +1,4 @@
 class MoviesApiEndpoint {
   static const String listMovies = "list_movies.json";
+  static const String apiMovieDetails = "/api/v2/movie_details.json";
 }
--- lib/logic/API/movies_api/api_manager/movies_api_manager.dart
+++ lib/logic/API/movies_api/api_manager/movies_api_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:team_flutter_6_movie_app/Model_Api/movie_details_model.dart';
 import 'package:team_flutter_6_movie_app/logic/API/movies_api/api_manager/movies_api_constant.dart';
 import 'package:team_flutter_6_movie_app/logic/API/movies_api/api_manager/movies_api_endpoint.dart';
 import 'package:team_flutter_6_movie_app/logic/API/movies_api/movies_model/movies_model.dart';
@@ -21,4 +22,28 @@ class MoviesApiManager {
       rethrow;
     }
   }
+}
+
+class MoviesDetailsApiManager{
+
+   Future<MovieDetailsResponse> getMoviesDetails({required String movieId})async{
+     Uri url = Uri.http(
+       MoviesApiConstant.serverName,
+       MoviesApiEndpoint.apiMovieDetails,
+       {
+         'movie_id': movieId
+       }
+     );
+     try{
+       var response = await http.get(url);
+       if(response.statusCode != 200){
+         throw Exception("Server Error: ${response.statusCode}");
+       }
+       var responseBody = response.body;
+       var json = jsonDecode(responseBody);
+       return MovieDetailsResponse.fromJson(json);
+     }catch(e){
+       rethrow;
+     }
+   }
 }
