fedbffcdc75f0f4998f3ee0e53b33bbd44660e8e
ahmed <ahmedsukkary52##@gmail.com>
2025-12-01 14:22:57 +0200

    Add Movie Details UI Screen (Again)

---
+++ lib/ui/Movie_Details/reusable_widgets/rate_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:team_flutter_6_movie_app/Utils/extension/extension.dart';
+import 'package:team_flutter_6_movie_app/Utils/text_app.dart';
+
+import '../../../Utils/color_App.dart';
+class RateWidget extends StatelessWidget {
+  final String imageName ;
+  final double rateNumber ;
+  const RateWidget({super.key,required this.imageName,required this.rateNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: context.height*0.05,
+      width: context.width*0.3,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: ColorApp.grayColor
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(imageName,fit: BoxFit.contain,width: context.width*0.08,),
+          Text('$rateNumber',style: TextApp.bold24White,)
+        ],
+      ),
+    );
+  }
+}
